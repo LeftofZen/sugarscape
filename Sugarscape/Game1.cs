7f495471f5eb509c2a45e422487a330018751b92
Benjamin Sutas <benjamin.sutas@wisetechglobal.com>
2021-02-17 16:07:35 +1100

    first commit

---
+++ Sugarscape/Game1.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sugarscape
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager _graphics;
+		SpriteBatch spriteBatch;
+		Grid<GoLCell> grid;
+		int updateCounter = 0;
+		int updateMod = 1;
+		Texture2D pixel;
+
+		public Game1()
+		{
+			_graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+			grid = new Grid<GoLCell>(10, 10);
+		}
+
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			pixel = new Texture2D(GraphicsDevice, 1, 1);
+			pixel.SetData(new Color[] { Color.White });
+
+			// TODO: use this.Content to load your game content here
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			if (updateCounter++ % updateMod == 0)
+			{
+				grid.Update();
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+			DrawGrid(gameTime, spriteBatch);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+
+
+		public void DrawGrid(Microsoft.Xna.Framework.GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch sb)
+		{
+			grid.ptrDraw.ForEachCell(
+				(cell, x, y) => 
+					sb.Draw(pixel, new Rectangle(1 + x * 32, 1 + y * 32, 31, 31), new Color(cell.GetLife(), cell.GetLife(), cell.GetLife())));
+		}
+	}
+}
---
+++ Sugarscape/Grid.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Sugarscape
+{
+	static class GridExtensions
+	{
+		public static void ForEachCell(this IGridCell[,] grid, Func<IGridCell> func)
+		{
+			for (var y = 0; y < grid.GetLength(0); ++y)
+			{
+				for (var x = 0; x < grid.GetLength(1); ++x)
+				{
+					grid[y, x] = func();
+				}
+			}
+		}
+
+		//public static void ForEachCell(this IGridCell[,] grid, Func<IGridCell, IGridCell> func)
+		//{
+		//	for (var y = 0; y < grid.GetLength(0); ++y)
+		//	{
+		//		for (var x = 0; x < grid.GetLength(1); ++x)
+		//		{
+		//			grid[y,x] = func(grid[y, x]);
+		//		}
+		//	}
+		//}
+
+		public static void ForEachCell(this IGridCell[,] grid, Func<IGridCell, int, int, IGridCell> func)
+		{
+			for (var y = 0; y < grid.GetLength(0); ++y)
+			{
+				for (var x = 0; x < grid.GetLength(1); ++x)
+				{
+					func(grid[y, x], x, y);
+				}
+			}
+		}
+
+		public static void ForEachCell(this IGridCell[,] grid, Action<IGridCell, int, int> action)
+		{
+			for (var y = 0; y < grid.GetLength(0); ++y)
+			{
+				for (var x = 0; x < grid.GetLength(1); ++x)
+				{
+					action(grid[y, x], x, y);
+				}
+			}
+		}
+	}
+
+	interface IGridCell
+	{
+		IGridCell Update();
+		int GetLife();
+	}
+
+	class GoLCell : IGridCell
+	{
+		const int maxLife = 255;
+		int life;
+
+		static readonly Random rnd = new Random(1);
+		public GoLCell()
+		{
+			life = rnd.Next(0, maxLife);
+		}
+
+		public int GetLife() => life;
+
+		public IGridCell Update()
+		{
+			life -= 1;
+			if (life <= 0)
+			{
+				life = maxLife;
+			}
+
+			// identity
+			return this;
+		}
+	}
+
+	class Grid<T> where T : IGridCell, new()
+	{
+		IGridCell[,] bufA;
+		IGridCell[,] bufB;
+
+		public IGridCell[,] ptrUpdate;
+		public IGridCell[,] ptrDraw;
+
+		public Grid(int sizeX, int sizeY)
+		{
+			bufA = new IGridCell[sizeY, sizeX];
+			bufB = new IGridCell[sizeY, sizeX];
+
+			bufA.ForEachCell(() => new T());
+			bufB.ForEachCell(() => new T());
+
+			ptrUpdate = bufA;
+			ptrDraw = bufB;
+		}
+
+		public void FlipBuffers()
+		{
+			var tmp = bufA;
+			bufA = bufB;
+			bufA = tmp;
+		}
+
+		public void Update()
+		{
+			ptrUpdate.ForEachCell((cell, x, y) => ptrDraw[y,x] = cell.Update());
+			FlipBuffers();
+		}
+	}
+}
