a95a7d0a8658d769528d09ebb6eef35e46bada00
Ben <bigbadbennybrother@gmail.com>
2021-02-18 13:40:57 +1100

    adds skeleton sugarscape cell

---
+++ Sugarscape/Cells/SSCell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sugarscape.Cells
+{
+	class SSCell : IGridCell
+	{
+		public SSCell()
+		{
+
+		}
+
+		public void Update(IGridCell[,] currentGrid, IGridCell newCell) => throw new NotImplementedException();
+
+		public bool Alive { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public (int x, int y) Location { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	}
+}
